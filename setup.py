92ab6ec33c87ac103e77507dc4f91c5c356d23e6
ashumishra288 <162741120+ashumishra288@users.noreply.github.com>
2024-05-09 22:59:18 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages,setup
+
+
+setup(
+name="mlproject",
+version='0.0.1',
+author='Ashish',
+author_email='ashumishra288.am@gmail.com',
+packages=find_packages(),
+install_requires.get_requirements('requiremtns.txt')
+)
+
+
+
+
+)
+
